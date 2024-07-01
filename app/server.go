c8c3ddba7316f231c48204e0f76362605450e99b
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2024-07-01 20:28:13 +0530

    pass stage

--- app/server.go
+++ app/server.go
@@ -7,17 +7,18 @@ import (
 )
 
 func main() {
+	fmt.Println("Logs from your program will appear here!")
+
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
-    fmt.Println("this is the err", err)
-    return;
+    conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+    conn.Write([]byte("HTTP/1.1 200 OK\n"))
 }
