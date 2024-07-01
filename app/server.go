5e6e6d1dee1e294b676ed6c082c7aac138db9685
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2024-07-01 20:31:31 +0530

    pass stage

--- app/server.go
+++ app/server.go
@@ -20,5 +20,5 @@ func main() {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-    conn.Write([]byte("HTTP/1.1 200 OK\r\n"))
+    conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 }
