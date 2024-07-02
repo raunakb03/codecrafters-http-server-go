ff71140f7f99e2c2c16ff3962a8cb2665caf9dc0
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2024-07-02 18:39:49 +0530

    pass stage

--- app/server.go
+++ app/server.go
@@ -31,6 +31,7 @@ func main() {
 	splitLine := strings.Split(line, " ")[1]
 	bodySplit := strings.Split(splitLine, "/")
 	if len(bodySplit) > 2 && bodySplit[1] == "echo" {
+        fmt.Println("exe first")
 		res := 
             fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(bodySplit[2]), bodySplit[2])
         conn.Write([]byte(res))
@@ -38,11 +39,13 @@ func main() {
 	}
 	fmt.Println(reqUrl)
 	if reqUrl == "" {
+        fmt.Println("exe second")
 		_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 		if err != nil {
 			panic(err)
 		}
 	} else {
+        fmt.Println("exe third")
 		_, err = conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 		if err != nil {
 			panic(err)
