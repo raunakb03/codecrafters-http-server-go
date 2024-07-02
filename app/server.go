9cd2a287c019c7fc674ad452f7e5029e24a26889
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2024-07-02 18:52:33 +0530

    pass stage

--- app/server.go
+++ app/server.go
@@ -30,25 +30,27 @@ func main() {
 	reqUrl := strings.Split(line, " ")[1][1:]
 	splitLine := strings.Split(line, " ")[1]
 	bodySplit := strings.Split(splitLine, "/")
+	fmt.Println("this is the body split ", bodySplit)
 	if len(bodySplit) > 2 && bodySplit[1] == "echo" {
-		fmt.Println("exe first")
 		res := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(bodySplit[2]), bodySplit[2])
-		conn.Write([]byte(res))
-		return
+		fmt.Println("this is the response ", res)
+		_, err = conn.Write([]byte(res))
+        if err != nil {
+            panic(err)
+        }
 	} else {
-	fmt.Println(reqUrl)
-	if reqUrl == "" {
-		fmt.Println("exe second")
-		_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-		if err != nil {
-			panic(err)
-		}
-	} else {
-		fmt.Println("exe third")
-		_, err = conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
-		if err != nil {
-			panic(err)
+		if reqUrl == "" {
+			fmt.Println("exe second")
+			_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+			if err != nil {
+				panic(err)
+			}
+		} else {
+			fmt.Println("exe third")
+			_, err = conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+			if err != nil {
+				panic(err)
+			}
 		}
 	}
-}
 }
