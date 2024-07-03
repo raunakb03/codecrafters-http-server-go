ecc90a909f9ee6f3d9e562098b8a117a4ce1c5c6
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2024-07-03 11:40:14 +0530

    pass stage

--- app/server.go
+++ app/server.go
@@ -4,52 +4,57 @@ import (
 	"bufio"
 	"fmt"
 	"net"
+	"net/http"
 	"os"
 	"strings"
 )
 
+func handleError(err error, message string) {
+	if err != nil {
+		fmt.Println(message)
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func sendResponse(conn net.Conn, res string) {
+	_, err := conn.Write([]byte(res))
+	handleError(err, "Failed to send response")
+}
+
+func handleUserAgent(conn net.Conn, req *http.Request) {
+    header := req.Header.Get("User-Agent")
+    fmt.Println(header)
+    res := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(header), header)
+    sendResponse(conn, res)
+
+}
+
 func main() {
 	fmt.Println("Logs from your program will appear here!")
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
-	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
-		os.Exit(1)
-	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
-	reader := bufio.NewReader(conn)
-	line, err := reader.ReadString('\n')
-	if err != nil {
-		panic(err)
-	}
+	handleError(err, "Failed to bind to port 4221")
 
-	reqUrl := strings.Split(line, " ")[1][1:]
-	splitLine := strings.Split(line, " ")[1]
-	bodySplit := strings.Split(splitLine, "/")
-	fmt.Println("this is the body split ", bodySplit)
-	if len(bodySplit) > 2 && bodySplit[1] == "echo" {
-		res := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(bodySplit[2]), bodySplit[2])
-		fmt.Println("this is the response ", res)
-		_, err = conn.Write([]byte(res))
-		if err != nil {
-			panic(err)
-		}
+	conn, err := l.Accept()
+	handleError(err, "Failed to accept connection")
+
+	reader := bufio.NewReader(conn)
+	req, err := http.ReadRequest(reader)
+	handleError(err, "Failed to read request")
+
+	reqUrl := req.URL.Path[1:]
+	splitLine := strings.Split(reqUrl, "/")
+
+	if reqUrl == "user-agent" {
+		handleUserAgent(conn, req)
+	} else if len(splitLine) > 1 && splitLine[0] == "echo" {
+		res := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(splitLine[1]), splitLine[1])
+		sendResponse(conn, res)
 	} else {
 		if reqUrl == "" {
-			fmt.Println("exe second")
-			_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-			if err != nil {
-				panic(err)
-			}
+			sendResponse(conn, "HTTP/1.1 200 OK\r\n\r\n")
 		} else {
-			fmt.Println("exe third")
-			_, err = conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
-			if err != nil {
-				panic(err)
-			}
+			sendResponse(conn, "HTTP/1.1 404 Not Found\r\n\r\n")
 		}
 	}
 }
